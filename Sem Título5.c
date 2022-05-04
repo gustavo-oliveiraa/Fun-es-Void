55c4f651e6428f84d8a2818b4253f621bea70651
Gustavo <80643129+gustavo-oliveiraa@users.noreply.github.com>
2022-05-04 10:43:16 -0300

    Add files via upload

---
+++ Sem Título5.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+void soma_notas(float , float);
+
+int main(){
+	
+	float n1, n2;
+	
+	
+	printf("Digite a nota 1: ");
+	scanf("%f", &n1);
+	
+	printf("Digite a nota 2: ");
+	scanf("%f", &n2);
+	
+	soma_notas(n1, n2);
+	
+	}
+	
+	void soma_notas(float n1, float n2){
+		printf("A soma das notas e %f", n1 + n2);
+	}
